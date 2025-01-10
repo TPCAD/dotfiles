ade3432a0886deb310757f189bc45bae2115fb81
TPCAD <1814083420@qq.com>
2025-01-10 22:03:12 +0800

    feat(yazi): display date of file in status line

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -3,3 +3,29 @@ require("full-border"):setup({
 	type = ui.Border.ROUNDED,
 	-- type = ui.Border.PLAIN,
 })
+
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ""
+	end
+
+	return ui.Line({
+		ui.Span((os.date("%Y/%m/%d %H:%M", tostring(h.cha.mtime):sub(1, 10)))):fg("magenta"),
+		" ",
+	})
+end, 500, Status.RIGHT)
+
+-- Status:children_add(function()
+-- 	local h = cx.active.current.hovered
+-- 	if h == nil or ya.target_family() ~= "unix" then
+-- 		return ""
+-- 	end
+--
+-- 	return ui.Line({
+-- 		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+-- 		":",
+-- 		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+-- 		" ",
+-- 	})
+-- end, 500, Status.RIGHT)
